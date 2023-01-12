f718b176271ec7b9e34e48e22c772f4f0eb52190
MarcHartleib <marc.hartleib91@gmail.com>
2023-01-12 11:06:36 +0100

    tags added

--- FitHub_Backend/src/main/java/com/codecool/fithub_backend/tagRepository/Tag.java
+++ FitHub_Backend/src/main/java/com/codecool/fithub_backend/tagRepository/Tag.java
@@ -1,5 +1,15 @@
 package com.codecool.fithub_backend.tagRepository;
 
 public enum Tag {
-    
+    CHEST,
+    BICEPS,
+    TRICEPS,
+    FOREARM,
+    SHOULDERS,
+    UPPER_BACK,
+    LOWER_BACK,
+    GLUTES,
+    QUADS,
+    HAMSTRINGS,
+    CALVES
 }
