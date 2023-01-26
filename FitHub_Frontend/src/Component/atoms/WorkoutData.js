45f5dc1f8213f98c2f0ff7639ca6a4283c55579a
Natascha <natascha.greif@gmail.com>
2023-01-26 17:05:10 +0100

    Added template for workout page

---
+++ FitHub_Frontend/src/Component/atoms/WorkoutData.js
@@ -0,0 +1,17 @@
+import React from "react";
+import useFetch from "react-fetch-hook"
+
+export default function WorkoutData() {
+
+
+        return (
+            <div id="content" className="p-4 p-md-5 pt-5">
+                <h2 className="mb-4">Workouts</h2>
+
+                <span className="workouts">pushups</span>
+                <span className="workouts">romanian deadlift</span>
+
+
+            </div>)
+    }
+
