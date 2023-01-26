41f6d6103954c231cd6c7113b019a16372a6dbd2
Natascha <natascha.greif@gmail.com>
2023-01-26 19:37:55 +0100

    Exercises are now viewable on the workouts page

--- FitHub_Frontend/src/Component/atoms/WorkoutData.js
+++ FitHub_Frontend/src/Component/atoms/WorkoutData.js
@@ -2,16 +2,35 @@ import React from "react";
 import useFetch from "react-fetch-hook"
 
 export default function WorkoutData() {
-
-
+    const {isLoading, error, data} = useFetch("/exercises")
+    let fetchStatus = null;
+    if (isLoading) {
+        fetchStatus = "Loading . . .";
+    }
+    if (error) {
+        fetchStatus = "Error loading data";
+    }
+    if (fetchStatus !== null) {
+        return (
+            <div id="content" className="p-4 p-md-5 pt-5">
+                <h2 className="mb-4">Workouts</h2>
+                <div>
+                    {fetchStatus}
+                </div>
+            </div>
+        )
+    } else {
         return (
             <div id="content" className="p-4 p-md-5 pt-5">
                 <h2 className="mb-4">Workouts</h2>
 
-                <span className="workouts">pushups</span>
-                <span className="workouts">romanian deadlift</span>
-
+                <div className="workout-box">
+                    {data.map((entry, i) => {
+                        return <div key={i}
+                                    className="workouts">{entry.exercise.toLowerCase().replaceAll('_', ' ')}</div>;
+                    })}
+                </div>
 
             </div>)
     }
-
+}
